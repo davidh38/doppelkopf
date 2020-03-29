f2bfd93e55606286a92c0cc22888ae079d8d9f01
David H <lifeainteasy@web.de>
2020-03-29 03:03:01 +0200

    game data structure introduced

--- test.clj
+++ test.clj
@@ -5,6 +5,7 @@
 (defn who_won_trick [trick]
   (eval (read-string (read-line)) 
         )
+  )
 
 (defn play_card [players_cards2, curr_player]
   (println curr_player)
@@ -21,6 +22,7 @@
      ]
     )
   )
+
 (defn play_round [t myround]
   (let [round_players (first t)
         players2 (second t)
@@ -34,25 +36,57 @@
     ;  []
     ; ]
     )
+  ) 
+
+(defn share_card_to_player [game players_cards]
+  (assoc game 
+         :players 
+         (assoc 
+            (get game :players)
+            (first players_cards)
+                (assoc (get (get game :players) (first players_cards))
+                        :cards   
+                        (second players_cards) 
+                )
+          )
+  )       
+)
+
+(defn shuffle_and_share_cards [game]
+  (reduce share_card_to_player game (map vector (keys (get game :players))
+                                    (partition 2 (shuffle (get game :cards)))
+                                    )
+  )
+)
+
+(defn initialize []
+  ; init cards
+  (def cards '([0 :c], [1 :c],[2 :c], [3 :c], [0 :s], [1 :s], [2 :s], [3 :s]))
+  (def players '(:p1 :p2 :p3 :p4))
+
+  ;(def round_players (take 4 (drop (who_won_trick tricks) (cycle (keys players)))))
+  ; mix and share cards
+    {:players (zipmap players (repeat  {:cards () :tricks ()}))
+    :current_trick '()
+    :round_start_player :p1
+    :cards cards
+    }
   )
 
-(def tricks [])
-; init cards
-(def cards '([0 :c], [1 :c],[2 :c], [3 :c], [0 :s], [1 :s], [2 :s], [3 :s]))
-;(def players {:p1 [], :p2 [], :p3[], :p4[]})
-(def players {:p1 [], :p2 []}) ;, :p3 [], :p4 []})
-; cards initialize
-(def trick [])
-; mix cards
-(def mixed_cards (shuffle cards))
-; share cards
-(def cards '([0 :c], [1 :c],[2 :c], [3 :c], [0 :s], [1 :s], [2 :s], [3 :s]))
-(def players (zipmap (keys players) (map vec (partition 4 mixed_cards))))
-(def round_players (take 4 (drop (who_won_trick tricks) (cycle (keys players)))))
 
-(reduce play_round 
-        (conj 
-          (range 10)
-          [(take 2 (cycle (keys players))) players tricks]
+(defn play_game []
+  (shuffle_and_share_cards (initialize))
+  (reduce play_round 
+          (conj 
+            (range 10)
+            [(take 2 (cycle (keys players))) players []]
+            )
           )
-        )
+  )
+
+(defn shuffle_and_share_cards [game]
+  (reduce share_card_to_player game (map vector (keys (get game :players))
+                                      (partition 2 (shuffle (get game :cards))))
+))
+
+
