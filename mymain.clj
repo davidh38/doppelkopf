972decfdd48e6c7d08f9bdde3bde6b3cc0dd317e
David H <lifeainteasy@web.de>
2021-08-18 00:47:23 +0200

    Refactoring code

--- mymain.clj
+++ mymain.clj
@@ -2,7 +2,8 @@
   (:require [myio]))
 
 (defn who_won_trick [trick]
-  (eval (read-string (read-line))))
+  (eval (read-string (read-line)))
+  )
 
 (defn share_card_to_player [game players_cards]
   (assoc game
@@ -14,19 +15,20 @@
                  :cards
                  (second players_cards)))))
 
-(defn shuffle_and_share_cards [game]
-  (->>
+(defn set_round_count [game]
+  (conj (range (get game :round_count)) game))
+
+(defn shuffle_and_share_cards [game myshuffle]
    (reduce share_card_to_player game
            (map vector
                 (keys (get game :players))
                 (->>  (get game :cards)
-                      (myio/myshuffle)
-                      (partition 2))
-            )
-    )
-   (set_round_count)))
+                      (myshuffle)
+                      (partition 2)))))
 
-(defn play_card [game, curr_player]
+
+
+(defn play_card [game curr_player]
   (println curr_player)
   (println game)
   (let [played_card (myio/play_card_inp)]
@@ -38,29 +40,29 @@
               [:current_trick]
               (conj (get-in game [:current_trick]) played_card))))
 
-(defn play_rounds [game round_count]
-  (let [round_players [:p1 :p2 :p3 :p4]]
-        (reduce play_card
-                (assoc-in game [:current_trick] '()) round_players)
 
-   )
-
+(defn play_round [game round]
+    (reduce play_card (assoc-in game [:current_trick] '()) [:p1 :p2 :p3 :p4])
 )
 
-(defn set_round_count [game]
-  (conj (range (get game :round_count)) game))
-
-(defn play_game [game]
-  (->> (shuffle_and_share_cards game)
-       (reduce play_rounds)))
+(defn play_rounds [game]
+   (reduce play_round game (range (get game :round_count)))
+)
 
 (defn play_game []
-  (->>
+  (->
    (myio/initialize_cards_and_players)
-   (shuffle_and_share_cards)
-   (reduce play_rounds)))
+   (shuffle_and_share_cards myio/myshuffle)
+   ;(announce)
+   (play_rounds)
+   )
+)
 
 (play_game)
 
-
 ;Try cloning this template project and look at how the files are laid out and how the ns form works: github.com/io-tupelo/clj-template – Alan Thompson 59 mins ago
+;play_caerd
+;
+;play_rounds (play_round (play_card))
+;
+;
