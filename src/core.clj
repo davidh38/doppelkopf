271c4ecb52da206a9a4ded7a4202839d17597b2b
David H <lifeainteasy@web.de>
2022-08-24 20:01:06 +0200

    Update README

--- src/core.clj
+++ src/core.clj
@@ -1,8 +1,10 @@
 (ns core
-  (:require [io]))
+  (:require [io] [clojure.string :as str])
+  (:import  java.util.Date))
 
-(defn ja [] "ja")
-
+(defn ja []
+  (str/capitalize "ja")
+  "ja")
 
 (defn initialize-game [deck]
                                         ; init cards
@@ -15,39 +17,34 @@
      :round-start-player :p1
      :cards deck
      :round-count (/ (count deck) (count players))
-     :mode ""
-     }
-    )
-  )
-
+     :mode ""}))
 
 (defn is-trump? [card]
 
-  (contains? card  (into  (map vector (into (repeat 4 10) (repeat 4 11)) (cycle [:c :s :h :d]))
-                          [[10 :h] [9 :d] [14 :d] [15 :d]])  )
+  (some  #(= card %) (into  [[10 :h] [9 :d] [14 :d] [15 :d]] (map vector (into (repeat 4 10) (repeat 4 11)) (cycle [:c :s :h :d])))))
 
+(defn highest-card? [[card1 player1] [card2 player2]]
+
+                                        ;  (let order [10 :h] [9 :d] [14 :d] [15 :d]
+                                        ;
+                                        ;       (> (first card1) (first card2)))
   )
-
-
-
-(defn who-won-trick [{current-trick :current-trick :as game} ]
+(defn who-won-trick [{current-trick :current-trick :as game}]
                                         ;  (assoc game :players '())
                                         ;(assoc game :round-count 2)
                                         ; first served color
                                         ; anly the highest of the first person counts
                                         ; what is the highest trump
 
+  (print current-trick)
 
-  game
-  )
-
+  game)
 
 (defn assign-trick-to-players [game]
-  game
-  )
+  game)
 
 (defn share-card-to-player [game [player cards]]
-  (assoc-in game [:players player :cards ]
+  (assoc-in game [:players player :cards]
             cards))
 
 (defn share-cards-to-players [{players :players cards :cards :as game}]
@@ -71,21 +68,19 @@
      (assoc-in game [:players curr-player :cards]
                (remove #(= played-card %) (get-in game [:players curr-player :cards])))
 
-     (update :current-trick conj {curr-player played-card}))))
+     (update :current-trick conj [played-card curr-player]))))
 
 (defn create-deck []
 
-  [[9 :s], [9 :c],[10 :c], [11 :c], [10 :s], [11 :s], [12 :s], [13 :s], [10 :s], [12 :s], [11 :s], [13 :s]]
+                                        ;[[9 :s], [9 :c],[10 :c], [11 :c], [10 :s], [11 :s], [12 :s], [13 :s], [10 :s], [12 :s], [11 :s], [13 :s]]
+  [[9 :s], [9 :c],[10 :c], [11 :c]]
                                         ;create full deck
 
                                         ; real deck
                                         ;(map vector (take 40 (cycle (range 9 16) )) (cycle [:h :d :s :d]))
-
   )
-
 (defn trick-player-order [player]
-  (take 4 (drop-while #(not= % player) [:p1 :p2 :p3 :p4 :p1 :p2 :p3 :p4]))
-  )
+  (take 4 (drop-while #(not= % player) [:p1 :p2 :p3 :p4 :p1 :p2 :p3 :p4])))
 
 (defn play-game-reduce [io-play-card-inp io-shuffle-deck]
 
@@ -95,21 +90,15 @@
    (initialize-game)
    (share-cards-to-players)
    (announce)
-   ((fn play-game[game-init]
+   ((fn play-game [game-init]
       (reduce
-       (fn play-turn[game round-count]
+       (fn play-turn [game round-count]
          (->>
           (reduce
-           (partial play-card io-play-card-inp) (assoc game :current-trick {}) (trick-player-order (game :round-start-player)))
+           (partial play-card io-play-card-inp) (assoc game :current-trick []) (trick-player-order (game :round-start-player)))
           (who-won-trick)
-          (assign-trick-to-players)
-          )
-         )
-       game-init (range (game-init :round-count)))))
-   )
-
-  )
-
+          (assign-trick-to-players)))
+       game-init (range (game-init :round-count)))))))
 
                                         ;(defn play-game []
                                         ;
@@ -131,16 +120,14 @@
                                         ;          game-next
                                         ;          (recur (inc round) game-next))))))
 
-
                                         ;(play-game-reduce io/play-card-inp io/myshuffle)
 
-
-
-
-(let [values (atom [[13 :s] [12 :s] [11 :s] [12 :s] [13 :s] [12 :s] [11 :s] [12 :s] [13 :s] [12 :s] [11 :s] [12 :s]])]
-  (defn my-function2 []
+(print (apply str (take 10 (repeat "#"))))
+;;,[10 :c], [11 :c], [10 :s], [11 :s], [12 :s], [13 :s], [10 :s], [12 :s], [11 :s], [13 :s]
+(let [values (atom [[9 :s], [9 :c],[10 :s], [11 :s]])]
+  (defn test-function []
     (let [e (first @values)]
       (swap! values rest)
       e)))
 
-                                        ;(play-game-reduce my-function2 identity)
+(play-game-reduce test-function identity)
