d864f84bcec1221ac66d5a4891e64e83064a5dad
David H <lifeainteasy@web.de>
2022-12-14 05:07:30 +0100

    Make it  work

--- src/core.clj
+++ src/core.clj
@@ -1,47 +1,102 @@
+
+
 (ns core
   (:require [io] [clojure.string :as str])
   (:import  java.util.Date))
 
-(defn ja []
-  (str/capitalize "ja")
-  "ja")
 
-(defn initialize-game [deck]
-                                        ; init cards
+(defn remove-card-from-player [card players-cards]
+
+  (let [card-split (split-with #(not= card %) players-cards)]
+    (concat (first  card-split) (rest (second card-split)))
+    )
+  )
+
+
+(defn initialize-game
+  "Game initialization"
+  [deck]
   (let [players '(:p1 :p2 :p3 :p4)]
 
-                                        ;(def round-players (take 4 (drop (who-won_trick tricks) (cycle (keys players)))))
-                                        ; mix and share cards
     {:players (zipmap players (repeat  {:cards () :tricks ()}))
      :current-trick ()
      :round-start-player :p1
      :cards deck
      :round-count (/ (count deck) (count players))
-     :mode ""}))
+     :mode "normal"
+     :re-team nil
+     }))
 
 (defn is-trump? [card]
 
-  (some  #(= card %) (into  [[10 :h] [9 :d] [14 :d] [15 :d]] (map vector (into (repeat 4 10) (repeat 4 11)) (cycle [:c :s :h :d])))))
+  (some  #(= card %)
+         (into  [[10 :h] [9 :d] [14 :d] [15 :d]]
+                (map vector (into (repeat 4 10) (repeat 4 11)) (cycle [:c :s :h :d])))))
 
-(defn highest-card? [[card1 player1] [card2 player2]]
+(defn determine-re-team [game]
+  game
 
-                                        ;  (let order [10 :h] [9 :d] [14 :d] [15 :d]
-                                        ;
-                                        ;       (> (first card1) (first card2)))
+
   )
-(defn who-won-trick [{current-trick :current-trick :as game}]
-                                        ;  (assoc game :players '())
-                                        ;(assoc game :round-count 2)
-                                        ; first served color
-                                        ; anly the highest of the first person counts
-                                        ; what is the highest trump
 
-  (print current-trick)
+(defn collect-points [cards]
+  (->>
+   (for [[v x y z] cards] [(second v) (second x) (second y) (second z)])
+   (reduce (fn [c d ] (concat c d) ))
 
-  game)
+   )
 
-(defn assign-trick-to-players [game]
-  game)
+  )
+
+
+(defn card-value [serve-color card]
+  (case card
+    [10 :h] 20
+    [13 :c] 19
+    [13 :s] 18
+    [13 :h] 17
+    [13 :d] 16
+    [12 :c] 15
+    [12 :s] 14
+    [12 :h] 12
+    [12 :d] 11
+    [10 :d] 10
+    [9 :d] 9
+    [11 serve-color] 8
+    [10 serve-color] 7
+    [14 serve-color] 6
+    [9 serve-color] 5
+    0))
+
+(defn determine-trick-winner [cards]
+
+  (->>
+   (apply max-key (partial card-value (second (second (first cards))))
+          (map second cards))
+   (.indexOf (map second cards))
+   (get cards)
+   (first)))
+
+(defn assign-trick-to-winner [{current-trick :current-trick mode :mode :as game}]
+                                        ;(let [[n m] (split-with (partial not= :b) [:a :b :c :b :d])] (concat n (rest m)))
+  (if  (= mode "normal")
+    (do
+      (if (some? is-trump?)
+        (assoc
+         (update-in game [:players (determine-trick-winner current-trick) :tricks]  conj current-trick)
+         :round-start-player
+         (determine-trick-winner current-trick))
+
+        )
+                                        ;if no trump, highest non trump wins
+                                        ;highest first trump wins
+      )
+    game))
+
+(defn print-game [game]
+  (clojure.pprint/pprint game)
+  game
+  )
 
 (defn share-card-to-player [game [player cards]]
   (assoc-in game [:players player :cards]
@@ -55,81 +110,82 @@
                      (partition (/ (count cards)
                                    (count players)))))))
 
-(defn announce [game]
+(defn announce-game-type [game]
 
   game)
 
 (defn play-card [io-play-card-inp game curr-player]
-  (println curr-player)
-  (clojure.pprint/pprint game)
 
   (let [played-card (io-play-card-inp)]
     (->
      (assoc-in game [:players curr-player :cards]
-               (remove #(= played-card %) (get-in game [:players curr-player :cards])))
+               (remove-card-from-player played-card (get-in game [:players curr-player :cards])))
 
-     (update :current-trick conj [played-card curr-player]))))
+     (update :current-trick conj [curr-player played-card]))))
 
-(defn create-deck []
-
-                                        ;[[9 :s], [9 :c],[10 :c], [11 :c], [10 :s], [11 :s], [12 :s], [13 :s], [10 :s], [12 :s], [11 :s], [13 :s]]
-  [[9 :s], [9 :c],[10 :c], [11 :c]]
-                                        ;create full deck
-
+(defn create-deck
+  "Create a deck with 40 cards
+  9 - 9
+  10 - 10
+  11 - As
+  12 - jack
+  13 - queen
+  14 - king
+  "
+  []
+  [[10 :h],[10 :h] [9 :d],[9 :s] [10 :s],[10 :h], [11 :d] [11 :s]]
                                         ; real deck
                                         ;(map vector (take 40 (cycle (range 9 16) )) (cycle [:h :d :s :d]))
   )
+
 (defn trick-player-order [player]
   (take 4 (drop-while #(not= % player) [:p1 :p2 :p3 :p4 :p1 :p2 :p3 :p4])))
 
-(defn play-game-reduce [io-play-card-inp io-shuffle-deck]
-
+(defn play-game-reduce
+  "play a full game"
+  [io-play-card-inp io-shuffle-deck]
+  (println "-------------- Start game ----------------")
   (->>
    (create-deck)
    (io-shuffle-deck)
    (initialize-game)
    (share-cards-to-players)
-   (announce)
-   ((fn play-game [game-init]
-      (reduce
-       (fn play-turn [game round-count]
-         (->>
-          (reduce
-           (partial play-card io-play-card-inp) (assoc game :current-trick []) (trick-player-order (game :round-start-player)))
-          (who-won-trick)
-          (assign-trick-to-players)))
-       game-init (range (game-init :round-count)))))))
+                                        ;   (announce-game-type)
+                                        ;(determine-re-team)
 
-                                        ;(defn play-game []
-                                        ;
-                                        ;  (let [game-init
-                                        ;        (->>
-                                        ;         (io/initialize-cards-and-players)
-                                        ;         (shuffle-and-share-cards io/myshuffle)
-                                        ;         (announce))]
-                                        ;
-                                        ;    (loop [round 1 game game-init]
-                                        ;
-                                        ;      (let [game-next (loop [curr-player 1 game-next game]
-                                        ;                        (if (> curr-player 4)
-                                        ;                          game-next
-                                        ;                          (recur (inc curr-player)
-                                        ;                                 (play-card game-next (keyword (str "p" curr-player))))))]
-                                        ;
-                                        ;        (if (> round 10)
-                                        ;          game-next
-                                        ;          (recur (inc round) game-next))))))
+   ;)
+  ((fn play-game [game-init]
+     (reduce
+      (fn play-turn [game _]
+        (->>
+         (reduce
+          (partial play-card io-play-card-inp) (assoc game :current-trick []) (trick-player-order (game :round-start-player)))
+         (assign-trick-to-winner)
+         (print-game)
+         ))
+      game-init (range (game-init :round-count)))))
 
-                                        ;(play-game-reduce io/play-card-inp io/myshuffle)
+  )
+)
 
-(println (apply str (take 10 (repeat "#"))))
-;;,[10 :c], [11 :c], [10 :s], [11 :s], [12 :s], [13 :s], [10 :s], [12 :s], [11 :s], [13 :s]
-(let [values (atom [[9 :s], [9 :c],[10 :s], [11 :s]])]
-  (defn test-function []
-    (let [e (first @values)]
-      (swap! values rest)
-      e)))
+
+(let [values (atom [[10 :h], [9 :d],[10 :h], [11 :d], [10 :h], [9 :s],[10 :s], [11 :s]])]
+(defn test-function []
+  (let [e (first @values)]
+    (swap! values rest)
+    e)))
 
 (play-game-reduce test-function identity)
 
-;(println (test-function))
+
+
+                                        ;(defn mytest []
+                                        ;  (let [a
+                                        ;        '([[:p1 [10 :h]] [:p2 [9 :s]] [:p3 [10 :s]] [:p4 [11 :s]]]
+                                        ;          [[:p1 [10 :h]] [:p2 [9 :d]] [:p3 [10 :h]] [:p4 [11 :d]]])]
+                                        ;    a
+                                        ;    )
+                                        ;  )
+
+(def a '([[:p1 [10 :h]] [:p2 [9 :s]] [:p3 [10 :s]] [:p4 [11 :s]]]
+         [[:p1 [10 :h]] [:p2 [9 :d]] [:p3 [10 :h]] [:p4 [11 :d]]]))
--- test/test2/core_test.clj
+++ test/test2/core_test.clj
@@ -1,18 +1,45 @@
 (ns test.test2.core-test
   (:require [clojure.test :refer :all]
             [test.test2.myfixture :as t :refer [myclosure]]
+            [core :refer [play-game-reduce remove-card-from-player]]
             )
   )
 
 (use-fixtures :each (t/myclosure))
 
-(deftest aoeu1 []
-  (is (= "ja2" "ja2") )
+(deftest play-game-reduce-test []
+  (let [values (atom [[10 :h], [9 :d],[10 :h], [11 :d], [10 :h], [9 :s],[10 :s], [11 :s]])]
+    (defn test-function []
+      (let [e (first @values)]
+        (swap! values rest)
+        e)))
+
+  (is (= (play-game-reduce test-function identity)
+         {:players
+          {:p1 {:cards ([10 :h]), :tricks ()},
+           :p2 {:cards (), :tricks ()},
+           :p3 {:cards (), :tricks ()},
+           :p4 {:cards (), :tricks ()}},
+          :current-trick
+          [[:p1 [10 :s]] [:p2 [9 :s]] [:p3 [10 :s]] [:p4 [11 :s]]],
+          :round-start-player :p1,
+          :cards
+          [[10 :h] [10 :h] [9 :d] [9 :s] [10 :s] [10 :h] [11 :d] [11 :s]],
+          :round-count 2,
+          :mode "normal"}
+         )
+
+
+
+      ))
+
+
+(deftest remove-card-from-play-test []
+  (is (= (core/remove-card-from-player  [14 :d] [[14 :d] [9 :d] [14 :d]])
+         ([9 :d] [14 :d])))
   )
 
-(deftest aoeu3 []
-  (is (= "ja" "ja") )
-  )
+
 
 
                                         ;(deftest play-one-trick []
